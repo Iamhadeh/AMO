53cc0b75813b18dedfb55922afaceab93cabb7b2
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2024-10-10 16:15:09 +0100

    mobile responsiveness

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,8 +1,8 @@
 
 
-    function openInNewTab(url) {
-        window.open(url, '_blank').focus();
-      }
+    // function openInNewTab(url) {
+    //     window.open(url, '_blank').focus();
+    //   }
 
 
 function Project (props) { 
@@ -11,7 +11,7 @@ function Project (props) {
             <h3 className="project-title">{props.imgTitle}</h3>
             <img src={props.Img} alt={props.ImgAlt} className="project-img" />
             <p className="project-description">{props.projectDescription}</p>
-            <button onClick = {openInNewTab(props.projectLink)} > <a href= {openInNewTab(props.projectLink)}  > Check It Out</a></button>
+            <button onClick = {props.projectLink} > <a href= {props.projectLink}  > Check It Out</a></button>
         </div>
     )
 
